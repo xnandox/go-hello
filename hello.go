da0a57f296330d0c6c47071aed09a0d3b1c40023
xnandox <fernando.sianipar@profisien.co.id>
2019-09-09 22:57:32 +0700

    Update hello.go

--- hello.go
+++ hello.go
@@ -1,7 +1,39 @@
+//package main
+
+//import "fmt"
+
+//func main() {
+//    fmt.Println("Hello, World")
+//}
+
+
 package main
 
 import "fmt"
+import "net/http"
 
 func main() {
-    fmt.Println("Hello, World")
+    http.HandleFunc("/", handlerIndex)
+    http.HandleFunc("/index", handlerIndex)
+    http.HandleFunc("/hello", handlerHello)
+
+        var address = ":8080"
+        fmt.Printf("server started at %s\n", address)
+
+        server := new(http.Server)
+        server.Addr = address
+        err := server.ListenAndServe()
+        if err != nil {
+                fmt.Println(err.Error())
+           }
+}
+
+func handlerIndex(w http.ResponseWriter, r *http.Request) {
+    var message = "Welcome"
+    w.Write([]byte(message))
+}
+
+func handlerHello(w http.ResponseWriter, r *http.Request) {
+    var message = "Hello World!"
+    w.Write([]byte(message))
 }
